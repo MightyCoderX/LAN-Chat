722601b6244d8825fcd8336fde56c26bc22d4ae9
Mattia Cardile <mattiaca003@gmail.com>
2021-10-26 17:33:58 +0200

    Updated server.js port selection based on environment variables

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const io = require('socket.io')(server);
 const messages = [];
 let users = [];
 
-server.listen(3000, () =>
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () =>
 {
-    console.log('Listening at "http://localhost:3000/"...');
+    console.log(`Listening at http://localhost:${PORT}/...`);
 });
 
 app.use(express.static('./client/'));
